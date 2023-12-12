76b49eef213ead3918415b5dc7078f24598fcfe2
daleogont <126600809+daleogont@users.noreply.github.com>
2023-12-12 23:57:52 +0200

    Add files via upload

---
+++ hello1.h
@@ -0,0 +1,7 @@
+
+#ifndef HELLO1_H
+#define HELLO1_H
+
+void print_hello2(unsigned int count);
+
+#endif
---
+++ hello2.c
@@ -0,0 +1,15 @@
+
+#include <linux/module.h>
+#include <linux/printk.h>
+
+MODULE_AUTHOR("Gontar Daniil <gontardaniil@gmail.com>");
+MODULE_DESCRIPTION("Hello, world in Linux Kernel Training");
+MODULE_LICENSE("Dual BSD/GPL");
+
+void print_hello2(unsigned int count) {
+    while (count > 0) {
+        pr_info("Hello, world from hello2!\n");
+        count--;
+    }
+}
+EXPORT_SYMBOL(print_hello2);
